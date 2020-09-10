be5d2630140279fcbd5bfb279b0c81c86374c473
Gisman4ik <Sftawitkakpro@gmail.com>
2020-09-10 23:58:52 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (typeof array === 'undefined') {
+        return 0;
+    }
+    let min = array[0] || 0;
+    for (let index = 0; index < array.length; index++) {
+        if (min>+array[index]) {
+            min=array[index];
+        };
+    }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (typeof array === 'undefined') {
+        return 0;
+    }
+    let min = array[0] || 0;
+    for (let index = 0; index < array.length; index++) {
+        if (min<+array[index]) {
+            min=array[index];
+        };
+    }
+  return min;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof array === 'undefined') {
+        return 0;
+    }
+    let avg = array.reduce((acc, curr) => acc + curr, 0) / array.length || 0;
+    return avg;
 }
